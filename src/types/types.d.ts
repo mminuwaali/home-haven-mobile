c286719069615c3f9d4369885863a0d5ba340f6f
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-07 14:31:51 +0100

    setup the project's type declarations for API

---
+++ src/types/types.d.ts
@@ -0,0 +1,10 @@
+interface IUser {
+    id: number;
+    email: string;
+    username: string;
+    last_name: string;
+    first_name: string;
+    last_login: string;
+    phone_number: string;
+    profile: null | string;
+}
