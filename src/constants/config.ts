743c96bca2a6867cac054b9dff87b09d19d650fe
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-07 15:19:39 +0100

    setup the welcome config data

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,1 +1,17 @@
-export const welcome: WelcomeType[] = [];
+export const welcome: WelcomeType[] = [
+    {
+        title: "Online Home Store and Furniture",
+        image: require("@/assets/images/welcome/welcome-1.png"),
+        description: "Discover all style and budgets of furniture, appliances, kitchen, and more from 500+ brands in your hand.",
+    },
+    {
+        title: "Delivery Right to Your Doorstep",
+        image: require("@/assets/images/welcome/welcome-2.png"),
+        description: "Sit back, and enjoy the convenience of our drivers delivering your order to your doorstep.",
+    },
+    {
+        title: "Get Support From Our Skilled Team",
+        image: require("@/assets/images/welcome/welcome-3.png"),
+        description: "If our products don't meet your expectations, we're available 24/7 to assist you.",
+    },
+];
