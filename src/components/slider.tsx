37ad26a7a37ff6ce2e0fddba59d67c6d8fa0b38c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-07 15:19:23 +0100

    updated the slider component

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,8 +1,10 @@
 import { Image, Text, View } from "react-native";
 
-export default function Slider(props: { width:number,welcome: WelcomeType }) {
-    return <View style={{ gap: 10 }} className="justify-center items-center w-full">
-        <Image source={props.welcome.image} className="w-full h-full object-contain" />
-        <Text className="text-blue text-lg">{props.welcome.title}</Text>
+export default function Slider(props: { welcome: WelcomeType }) {
+    return <View style={{ gap: 20}} className="justify-center h-full w-screen overflow-hidden">
+        <Image style={{transform: [{ scaleX: 1.2 }] }} source={props.welcome.image} className="w-full flex-1 object-cover rounded-b-full" />
+
+        <Text className="text-blue font-bold text-4xl px-6 mx-auto text-center">{props.welcome.title}</Text>
+        <Text className="text-blue text-lg px-6 mx-auto text-center">{props.welcome.description}</Text>
     </View>;
 };
