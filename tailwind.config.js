c733ec02c1ae3205a464d62c52da72d8b136ab12
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-07 15:18:46 +0100

    added some configurations in the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,37 @@ module.exports = {
         primary: "#156651",
         secondary: "#E3AD33",
       },
-      fontSize:{
-        h1:"40px",
-        h2:"32px",
-        h3:"24px",
-        h4:"18px",
-        h5:"16px",
-      }
+      fontSize: {
+        h1: "40px",
+        h2: "32px",
+        h3: "24px",
+        h4: "18px",
+        h5: "16px",
+      },
+      padding: {
+        "1/2": "50%",
+        "1/3": "33%",
+        "1/4": "25%",
+        "1/5": "20%",
+        "1/20": "5%",
+        "1/10": "10%",
+      },
+      margin: {
+        "1/2": "50%",
+        "1/3": "33%",
+        "1/4": "25%",
+        "1/5": "20%",
+        "1/20": "5%",
+        "1/10": "10%",
+      },
     },
   },
   plugins: [
     ({ addComponents }) => void addComponents({
+      ".wrapper": {
+        width: "100%",
+        height: "100%",
+      },
       ".cointainer": {
         width: "100%",
         padding: "5%",
