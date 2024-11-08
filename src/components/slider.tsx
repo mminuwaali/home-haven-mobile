15e425d9b489c5a3cf81be2f2b060a4b344fd21e
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-08 18:41:24 +0100

    created slider component for onboarding screen

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,10 +1,10 @@
 import { Image, Text, View } from "react-native";
 
-export default function Slider(props: { welcome: WelcomeType }) {
+export default function Slider(props: { welcome: IWelcome }) {
     return <View style={{ gap: 20}} className="justify-center h-full w-screen overflow-hidden">
         <Image style={{transform: [{ scaleX: 1.2 }] }} source={props.welcome.image} className="w-full flex-1 object-cover rounded-b-full" />
 
-        <Text className="text-blue font-bold text-4xl px-6 mx-auto text-center">{props.welcome.title}</Text>
-        <Text className="text-blue text-lg px-6 mx-auto text-center">{props.welcome.description}</Text>
+        <Text className="text-blue font-bold text-h1 px-6 mx-auto text-center">{props.welcome.title}</Text>
+        <Text className="text-blue text-h4 px-6 mx-auto text-center">{props.welcome.description}</Text>
     </View>;
 };
