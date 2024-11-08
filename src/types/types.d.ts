93700f7620a35083a09c0c499a3fd53467bedf25
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-08 18:38:56 +0100

    created interfaces for Product, Carousel and Cart

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -8,3 +8,37 @@ interface IUser {
     phone_number: string;
     profile: null | string;
 }
+
+interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    discount: number;
+    category: string;
+    created_at: string;
+    updated_at: string;
+    description: string;
+    image: IProductImage[];
+}
+
+interface IProductImage {
+    id: number;
+    url: string;
+}
+
+interface ICartItem {
+    id: number;
+    product: {
+        product: IProduct;
+        quantity: number;
+    };
+    total: number;
+    created_at: string;
+    updated_at: string;
+}
+
+interface ICarousel {
+    id: number;
+    image: string;
+}
