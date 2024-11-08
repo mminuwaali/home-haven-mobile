534d2bc42f265ec82718fa3f081e078aca404506
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-08 18:40:48 +0100

    setup a global styles file

---
+++ src/constants/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    shadow: {
+        elevation: 5,
+        shadowRadius: 3.84,
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+    },
+});
