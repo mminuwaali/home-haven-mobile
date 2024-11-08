af431a56691727e9834599880bbac30bcf003328
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-11-08 18:39:29 +0100

    defined some icons and images in the media file

---
+++ src/constants/media.ts
@@ -0,0 +1,4 @@
+export const googleIcon = require("@/assets/icons/social/google.png");
+export const facebookIcon = require("@/assets/icons/social/facebook.png");
+
+export const profileImage = require("@/assets/images/profile.png");
